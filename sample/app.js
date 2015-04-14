6f99d93c2f9367fbb9f1335d3cbd4cea6d9d8665
ackuser <mail@ackarim.com>
2015-04-14 10:35:04 +0100

    Adding angular-recaptcha

--- sample/app.js
+++ sample/app.js
@@ -1,10 +1,10 @@
 angular.module('validationApp',
-['credit-cards']
+['credit-cards','internationalPhoneNumber','vcRecaptcha']
 )
 .controller('mainController', function($scope, $http) {
 
   // function to submit the form after all validation has occurred
-  $scope.submitForm = function() {
+  $scope.submit = function() {
 
     // check to make sure the form is completely valid
     if ($scope.userForm.$valid) {
@@ -36,4 +36,48 @@ angular.module('validationApp',
 
   }
 
+})
+.controller('testCtrl', function ($scope, vcRecaptchaService) {
+    console.log("this is your app's controller");
+    $scope.response = null;
+    $scope.widgetId = null;
+
+    $scope.model = {
+        key: '=== REPLACE THIS WITH YOUR PUBLIC ReCaptcha KEY ==='
+    };
+
+    $scope.setResponse = function (response) {
+        console.info('Response available');
+
+        $scope.response = response;
+    };
+
+    $scope.setWidgetId = function (widgetId) {
+        console.info('Created widget ID: %s', widgetId);
+
+        $scope.widgetId = widgetId;
+    };
+
+    $scope.submit = function () {
+        var valid;
+
+        /**
+         * SERVER SIDE VALIDATION
+         *
+         * You need to implement your server side validation here.
+         * Send the reCaptcha response to the server and use some of the server side APIs to validate it
+         * See https://developers.google.com/recaptcha/docs/verify
+         */
+        console.log('sending the captcha response to the server', $scope.response);
+
+        if (valid) {
+            console.log('Success');
+        } else {
+            console.log('Failed validation');
+
+            // In case of a failed validation you need to reload the captcha
+            // because each response can be checked just once
+            vcRecaptchaService.reload($scope.widgetId);
+        }
+    };
 });
